651dc1c86166beb665e7fd0b21bae295c53d5715
devansh-dvj <devansh.dvj@gmail.com>
2022-03-01 13:45:12 +0530

    Cleaned and added comments to loaddb

--- work/dblayer/loaddb.c
+++ work/dblayer/loaddb.c
@@ -10,24 +10,17 @@
 #include "tbl.h"
 #include "util.h"
 
-#define checkerr(err)                                                          \
-  {                                                                            \
-    if (err < 0) {                                                             \
-      PF_PrintError();                                                         \
-      exit(1);                                                                 \
-    }                                                                          \
-  }
-
 #define MAX_PAGE_SIZE 4000
 
 #define DB_NAME "data.db"
 #define INDEX_NAME "data.db.2"
 #define CSV_NAME "data.csv"
 
-// extern void tperror2(int, char *);
-
-void tperror2(int status, char *s) {
-  // EXTRA FUNCTION: prints error
+/*
+  EXTRA FUNCTION
+  prints error
+*/
+void lerror(int status, char *s) {
   if (status < 0) {
     printf("%s\n", s);
     PF_PrintError();
@@ -35,64 +28,68 @@ void tperror2(int status, char *s) {
   }
 }
 
-/**
- * Takes a schema, and an array of strings (fields), and uses the functionality
- * in codec.c to convert strings into compact binary representations
- */
+/*
+  Takes a schema, and an array of strings (fields), and uses the functionality
+  in codec.c to convert strings into compact binary representations
+*/
 int encode(Schema *sch, char **fields, byte *record, int spaceLeft) {
   int n = sch->numColumns;
   int len = 0;
 
+  // For every column in schema
   for (int i = 0; i < n; ++i) {
-    // printf("%s: %s\n", sch->columns[i]->name, fields[i]);
     switch (sch->columns[i]->type) {
+
     case VARCHAR: {
+      // encode field value into 'size' bytes from record[len]
       int size = EncodeCString(fields[i], record + len, spaceLeft);
       len += size;
       spaceLeft -= size;
-      // printf("Size: %d\n", size);
-      // printf("Record: %s\n", record);
       break;
     }
+
     case INT: {
+      // encode field value into 'size' (4) bytes from record[len]
       int size = EncodeInt(atoi(fields[i]), record + len);
       len += size;
       spaceLeft -= size;
-      // printf("Record: %s\n", record);
       break;
     }
+
     case LONG: {
+      // encode field value into 'size' (8) bytes from record[len]
       int size = EncodeLong(atoll(fields[i]), record + len);
       len += size;
       spaceLeft -= size;
-      // printf("Record: %s\n", record);
       break;
     }
+
     default:
-      // printf("Unknown type %d\n", sch->columns[i]->type);
       break;
     }
   }
 
+  // If no space is left
   if (spaceLeft <= 0) {
     printf("Not enough space left\n");
     return -1;
   }
 
+  // Return length of the record
   return len;
 }
 
 Schema *loadCSV() {
-
   int status;
 
-  // Open csv file, parse schema
+  // Open csv file
   FILE *fp = fopen(CSV_NAME, "r");
   if (!fp) {
     perror("data.csv could not be opened");
     exit(EXIT_FAILURE);
   }
 
+  // Get first line
   char buf[MAX_LINE_LEN];
   char *line = fgets(buf, MAX_LINE_LEN, fp);
   if (line == NULL) {
@@ -100,45 +97,57 @@ Schema *loadCSV() {
     exit(EXIT_FAILURE);
   }
 
-  // Open main db file
+  // Parse Schema
   Schema *sch = parseSchema(line);
   Table *tbl;
 
+  // Open main db file
   status = Table_Open(DB_NAME, sch, true, &tbl);
-  tperror2(status, "LoadDB: error while opening table\n");
+  lerror(status, "LoadDB: error while opening table\n");
 
+  // Create secondary index
   status = AM_DestroyIndex(DB_NAME, 2);
   status = AM_CreateIndex(DB_NAME, 2, 'i', 4);
-  tperror2(status, "LoadDB: error while creating index\n");
+  lerror(status, "LoadDB: error while creating index\n");
 
+  // Open index file FD
   int indexFD = PF_OpenFile(INDEX_NAME);
-  tperror2(status, "LoadDB: error while opening index file\n");
+  lerror(status, "LoadDB: error while opening index file\n");
 
   char *tokens[MAX_TOKENS];
   char record[MAX_PAGE_SIZE];
 
   while ((line = fgets(buf, MAX_LINE_LEN, fp)) != NULL) {
 
+    // Split every line into tokens
     int n = split(line, ",", tokens);
     assert(n == sch->numColumns);
+
+    // Encode the tokens into record of 'len' bytes
     int len = encode(sch, tokens, record, sizeof(record)); // in bytes
 
+    // Insert the record into the table with 'rid' as record ID
     RecId rid;
     status = Table_Insert(tbl, record, len, &rid);
-    tperror2(status, "LoadDB: error while inserting into table\n");
+    lerror(status, "LoadDB: error while inserting into table\n");
 
     printf("RID: %i | Length (bytes): %d | Data: %s\n", rid, len, line);
     fflush(stdout);
 
+    // Use population as secondary index to insert into index file
     int population = atoi(tokens[2]);
-    status = AM_InsertEntry(indexFD, 'i', 4, &population, rid);
-    tperror2(status, "LoadDB: error while inserting into index file\n");
+    status = AM_InsertEntry(indexFD, 'i', 4, (char *)&population, rid);
+    lerror(status, "LoadDB: error while inserting into index file\n");
   }
 
+  // Close index file FD
   status = PF_CloseFile(indexFD);
-  tperror2(status, "LoadDB: error while closing index file\n");
+  lerror(status, "LoadDB: error while closing index file\n");
 
+  // Close main db file
   Table_Close(tbl);
+
+  // Close csv file
   fclose(fp);
 
   return sch;
