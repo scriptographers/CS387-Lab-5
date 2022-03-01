1610ce44b7db780a826289a3343f48fac80726f5
devansh-dvj <devansh.dvj@gmail.com>
2022-03-01 15:29:21 +0530

    Added Submission tarball

--- 190100044_190100055/amlayer/amglobals.c
+++
@@ -1,6 +0,0 @@
-# include "am.h"
-
-int AM_RootPageNum = 0;
-int AM_LeftPageNum = 0;
-int AM_Errno;
-
--- 190100044_190100055/dblayer/util.h
+++
@@ -1,9 +0,0 @@
-#define MAX_TOKENS 100
-#define MAX_LINE_LEN 1000
-
-int stricmp(char const *a, char const *b);
-char *trim(char *str);
-
-int split(char *buf, char *delim, char **tokens);
-
-Schema *parseSchema(char *buf);
